568b7c5da50681db7a60c37f48d42b8a5d94d8b0
louay-hesham <louay.hesham@gmail.com>
2017-03-25 22:20:34 +0200

    Basic formats hierarchy

---
+++ src/Formats/Format1.java
@@ -0,0 +1,7 @@
+package Formats;
+
+/**
+ * Created by louay on 3/25/2017.
+ */
+public class Format1 extends Format {
+}
---
+++ src/Formats/Format2.java
@@ -0,0 +1,7 @@
+package Formats;
+
+/**
+ * Created by louay on 3/25/2017.
+ */
+public class Format2 extends Format {
+}
