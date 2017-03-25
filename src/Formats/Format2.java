78708400c1b0286c9b420a5930e0fdafc306f028
louay-hesham <louay.hesham@gmail.com>
2017-03-26 00:49:34 +0200

    updated classes hierarchy

--- src/Formats/Format2.java
+++
@@ -1,7 +0,0 @@
-package Formats;
-
-/**
- * Created by louay on 3/25/2017.
- */
-public class Format2 extends Format {
-}
--- src/Formats/Format3.java
+++
@@ -1,7 +0,0 @@
-package Formats;
-
-/**
- * Created by louay on 3/25/2017.
- */
-public class Format3 extends Format {
-}
