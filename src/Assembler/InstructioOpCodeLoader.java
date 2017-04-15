5ed69672438cfcbfde081ad03e4489a6138f71ef
tifa64 <krietallo@gmail.com>
2017-04-15 17:34:26 +0200

    Get opcodes with Instance of class Instrction

--- src/Assembler/InstructioOpCodeLoader.java
+++
@@ -1,8 +0,0 @@
-package Assembler;
-
-/**
- * Created by Krietallo on 4/15/2017.
- */
-public class InstructioOpCodeLoader {
-
-}
---
+++ src/Assembler/Instruction.java
@@ -0,0 +1,18 @@
+package Assembler;
+
+/**
+ * Created by Krietallo on 4/15/2017.
+ */
+public class Instruction {
+
+    public final String inst, frmt, opcode;
+    public final int int_opcode;
+
+    public Instruction(String inst, String frmt, String opcode) {
+        this.inst = inst;
+        this.frmt = frmt;
+        this.opcode = opcode;
+        int_opcode = Integer.parseInt(opcode, 16);
+    }
+
+}
