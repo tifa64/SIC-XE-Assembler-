412cc5999f76a6f41d7a6e2dfc1eb5a9b7e19a58
tifa64 <krietallo@gmail.com>
2017-04-15 18:39:10 +0200

    Forma 1,2 & comment

--- src/Assembler/Instruction.java
+++ src/Assembler/Instruction.java
@@ -1,5 +1,9 @@
 package Assembler;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+
 /**
  * Created by Krietallo on 4/15/2017.
  */
@@ -8,11 +12,16 @@ public class Instruction {
     public final String inst, frmt, opcode;
     public final int int_opcode;
 
+
     public Instruction(String inst, String frmt, String opcode) {
         this.inst = inst;
         this.frmt = frmt;
         this.opcode = opcode;
         int_opcode = Integer.parseInt(opcode, 16);
+
     }
+
+
+
 
 }
--- src/Assembler/Line/Format2.java
+++ src/Assembler/Line/Format2.java
@@ -1,5 +1,7 @@
 package Assembler.Line;
 
+import Assembler.InstructionSetLoader;
+
 /**
  * Created by louay on 3/25/2017.
  */
@@ -22,6 +24,20 @@ public class Format2 extends Format {
 
     @Override
     public String getObjectCode() {
-        return null;
+        InstructionSetLoader temp = InstructionSetLoader.getLoader();
+        String instOpCode = temp.getInstOpCode(mnemonic);
+
+        String r1 = operand.split(",")[0];
+        String r2;
+        int r11 = temp.getRegOpCode(r1);
+        if(r1.length() != operand.length())
+        {
+            r2 = operand.split(",")[1];
+            int r22 = temp.getRegOpCode(r2);
+        }
+        else {
+            r2 = "0";
+        }
+        return instOpCode+r1+r2;
     }
 }
