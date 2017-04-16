a9b95fd9d7c87c420f2481d5ba1f243c08198f6d
louay-hesham <louay.hesham@gmail.com>
2017-04-16 20:00:43 +0200

    further code cleaning

--- src/Assembler/Pass2.java
+++ src/Assembler/Pass2.java
@@ -2,7 +2,6 @@ package Assembler;
 
 import Assembler.Line.AssemblyLine;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,7 +17,7 @@ public class Pass2 {
 
     private static int recordSize = 0;
     private static boolean startFlag = false;
-    private static String currentObjCode = new String();
+    private static String currentObjCode = "";
     private static StringBuilder TRecordsSB = new StringBuilder();
     private static StringBuilder objCodeSB = new StringBuilder();
     private static String programStart = Integer.toHexString(Pass1.programStart).toUpperCase();
@@ -26,34 +25,35 @@ public class Pass2 {
     public static void generateObjectCodes() {
         try {
             ArrayList<String> fileLines = new ArrayList<>();
-            /**Case RESW or RESB**/
+            /*Case RESW or RESB**/
             for (AssemblyLine al : Pass1.assemblyLines) {
                 try {
                     currentObjCode = al.getObjectCode();
                 } catch (Exception m) {
-                    TRecordsSB.append(Integer.toHexString(recordSize / 2).toUpperCase() + " ");
+                    TRecordsSB.append(Integer.toHexString(recordSize / 2).toUpperCase()).append(" ");
                     fileLines.add("T" + " " + TRecordsSB.toString() + objCodeSB.toString());
                     TRecordsSB = new StringBuilder();
                     objCodeSB = new StringBuilder();
                     recordSize = 0;
                 }
 
-                /**Case START**/
+                /*Case START**/
                 if (currentObjCode.startsWith("H")) {
                     fileLines.add(currentObjCode);
                 }
-                /**Case END**/
-                /**It will add the last line of T before it then will the END line**/
+                /*Case END**/
+                /*It will add the last line of T before it then will the END line**/
                 else if (currentObjCode.startsWith("E")) {
-                    TRecordsSB.append(Integer.toHexString(recordSize / 2).toUpperCase() + " ");
+                    TRecordsSB.append(Integer.toHexString(recordSize / 2).toUpperCase()).append(" ");
                     fileLines.add("T" + " " + TRecordsSB.toString() + objCodeSB.toString());
                     fileLines.add(currentObjCode);
                     break;
                 }
-                /**Case Comment**/
-                else if (currentObjCode.length() == 0)
+                /*Case Comment**/
+                else if (currentObjCode.length() == 0) {
                     continue;
-                /**Other wise it will continue the T record till the sz is bigger than 30 bytes (60 characters)**/
+                }
+                /*Other wise it will continue the T record till the sz is bigger than 30 bytes (60 characters)**/
                 else {
                     if (!startFlag) {
                         if (programStart.length() % 2 == 1) {
@@ -62,11 +62,11 @@ public class Pass2 {
                             sbTemp.append(programStart);
                             programStart = sbTemp.toString();
                         }
-                        objCodeSB.append(programStart + " ");
+                        objCodeSB.append(programStart).append(" ");
                         recordSize += programStart.length();
                         startFlag = true;
                     } else if (recordSize + currentObjCode.length() <= 60) {
-                        /**Because  when new T record is begun it ignores the last Object Code of the previous T record**/
+                        /*Because  when new T record is begun it ignores the last Object Code of the previous T record**/
                         if (recordSize == 0) {
                             recordSize += 1;
                             continue;
@@ -75,9 +75,9 @@ public class Pass2 {
                             recordSize--;
                         }
                         recordSize += currentObjCode.length();
-                        objCodeSB.append(currentObjCode + " ");
+                        objCodeSB.append(currentObjCode).append(" ");
                     } else {
-                        TRecordsSB.append(Integer.toHexString(recordSize / 2).toUpperCase() + " ");
+                        TRecordsSB.append(Integer.toHexString(recordSize / 2).toUpperCase()).append(" ");
                         fileLines.add("T" + " " + TRecordsSB.toString() + objCodeSB.toString());
                         TRecordsSB = new StringBuilder();
                         objCodeSB = new StringBuilder();
@@ -87,8 +87,6 @@ public class Pass2 {
             }
             Path listingFile = Paths.get("HTME.txt");
             Files.write(listingFile, fileLines, Charset.forName("UTF-8"));
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
