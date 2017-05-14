a02cfd2cf026d1926bdece4013efd471121bb848
louay-hesham <louay.hesham@gmail.com>
2017-05-14 21:44:59 +0200

    csect string variable

--- src/AssemblerCore/Line/Directive.java
+++ src/AssemblerCore/Line/Directive.java
@@ -142,10 +142,12 @@ public class Directive extends AssemblyLine {
     @Override
     public String getObjectCode() throws Exception {
         switch (mnemonic) {
-            case "START":
+            case "START": {
+                Pass2.csect = this.label;
                 return "H" + " " + this.label +
                         " " + Pass2.padStringWithZeroes(this.operand, 6) +
                         " " + Pass2.padStringWithZeroes(Integer.toHexString(Pass1.programLength), 6);
+            }
             case "END":
                 return ("E" + " " + Pass2.padStringWithZeroes(Integer.toHexString(Pass1.programStart), 6));
             case "RESB":
@@ -197,6 +199,10 @@ public class Directive extends AssemblyLine {
             case "LTORG":
             case "EQU":
             case "ORG": {
+                return "";
+            }
+            case "CSECT": {
+                Pass2.csect = this.label;
                 return "";
             }
             default:
